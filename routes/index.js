5afee291ca5610ade89bb7563fd0e0ca61e3028e
Mohsin <1mohsinihsan@gmail.com>
2023-12-19 17:42:37 +0500

    login-logout-mongodb-mangoose-express

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,61 @@
 var express = require('express');
 var router = express.Router();
 const userModel = require("./users")
+const passport = require('passport');
+const localStrategy = require('passport-local');
 
-router.get('/create', async function (req, res){
- const userData =  await userModel.create({
-    username: "awais",
-    nickname : "awaisooo",
-    description: "I love to code in any language, node and react",
-    categories: ['java', 'android', 'selenium', 'automation', 'js', 'Node'],
-  })
-  res.send(userData)
+passport.use(new localStrategy(userModel.authenticate()))
+
+router.get('/', function (req, res) {
+  res.render('index');
 })
+
+router.get('/profile', isLoggedIn, function(req, res)  {
+  res.render('profile')
+})
+
+router.post('/register', function(req, res) {
+  var userdata = new userModel({
+    username: req.body.username,
+    secret: req.body.secret
+  })
+  userModel.register(userdata, req.body.password)
+  .then(function(registereduser){
+    passport.authenticate("local")(req,res, function(){
+      res.redirect('/profile')
+    })
+  })
+})
+
+
+router.post('/login', passport.authenticate("local", {
+  successRedirect:"/profile",
+  failureRedirect: "/"
+}), function(req, res) {})
+
+router.get('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    if (err) {return next(err);}
+    res.redirect('/')
+  })
+})
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+  res.redirect('/')
+}
+
+// router.get('/create', async function (req, res){
+//  const userData =  await userModel.create({
+//     username: "awais",
+//     nickname : "awaisooo",
+//     description: "I love to code in any language, node and react",
+//     categories: ['java', 'android', 'selenium', 'automation', 'js', 'Node'],
+//   })
+//   res.send(userData)
+// })
 // searching faiq case insensitive
 // router.get('/find', async (req, res) => {
 //   var regex = new RegExp('Faiq', 'i');
@@ -37,18 +82,18 @@ router.get('/create', async function (req, res){
 
 //searching a specific length field
 
-router.get('/find', async (req, res) => {
-  // var regex = new RegExp('Faiq', 'i');
-  let user = await userModel.find({
-    $expr: {
-      $and: [
-        { $gte: [{ $strLenCP: '$nickname' }, 0] },
-        { $lte: [{ $strLenCP: '$nickname' }, 12] }
-    ]
-  }
-})
-  res.send(user)
-})
+// router.get('/find', async (req, res) => {
+//   // var regex = new RegExp('Faiq', 'i');
+//   let user = await userModel.find({
+//     $expr: {
+//       $and: [
+//         { $gte: [{ $strLenCP: '$nickname' }, 0] },
+//         { $lte: [{ $strLenCP: '$nickname' }, 12] }
+//     ]
+//   }
+// })
+//   res.send(user)
+// })
 
 
 //search in the date range
--- routes/users.js
+++ routes/users.js
@@ -16,19 +16,29 @@
 // })
 // module.exports = mongoose.model("user", userschema)
 
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb://127.0.0.1:27017/practice-test');
+// const userschema = mongoose.Schema({
+//   username: String,
+//   nickname : String,
+//   description: String,
+//   categories: {
+//     type: Array,
+//     default: []
+//   },
+//   datecreated: {
+//     type: Date,
+//     default: Date.now()
+//   }
+// })
+// module.exports = mongoose.model("user", userschema)
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://127.0.0.1:27017/practice-test');
+const plm = require('passport-local-mongoose')
+mongoose.connect('mongodb://127.0.0.1:27017/testingendgame2');
 const userschema = mongoose.Schema({
-  username: String,
-  nickname : String,
-  description: String,
-  categories: {
-    type: Array,
-    default: []
-  },
-  datecreated: {
-    type: Date,
-    default: Date.now()
-  }
+  username : String,
+  password : String,
+  secret : String
 })
+userschema.plugin(plm)
 module.exports = mongoose.model("user", userschema)
